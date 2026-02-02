23dbcb2e92554dcbe0b8dfc768e28e404210a1e9
umutcanbastepe <umutcanbastepe03@gmail.com>
2026-02-02 14:24:37 +0300

    .Net MVC projesi olusturuldu

---
+++ RequestManagementSystem/RequestManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RequestManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
